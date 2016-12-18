23cdb8282aa1c8b1169e3c371f7e936ae73fa847
sheson <no@lol.comn>
2016-12-18 13:11:43 +0100

    LODGen 1.0
    added support for dds in export file, it will be used to create 2 flat surfaces for tree LOD
    	uses FlatTextures= for x-width, y-height, z-shift, scale, lighting effect 1, path to normals file
    added support for AltTextures= replacement texture list (list of 3D names and textures), applied before atlas
    added support reading FO4 LOD nif and BGSM (BGSM always trumps data in nif)
    added BA2 support, GNRL only
    added export Mode=FO4, see updates to LODGen.pas for new export fields for SCOL data, enable parent form id, replacement textures, etc
    added export Mode=Merge5 to combine several nif into one
    read more block types typically used for trees to convert to statics

---
+++ Common/BSVertexData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LODGenerator.Common
+{
+    [Serializable]
+    public class BSVertexData
+    {
+        public Vector3 vertex;
+        public float bitangentX;
+        public ushort unknownShort1;
+        public uint unknownInt1;
+        public UVCoord uvcoords;
+        public Vector3 normal;
+        public float bitangentY;
+        public Vector3 tangent;
+        public float bitangentZ;
+        public Color4 vertexColors;
+        public float[] boneWeights;
+        public byte[] boneIndices;
+        public uint unknownInt2;
+
+        public BSVertexData()
+        {
+            this.vertex = new Vector3(0.0f, 0.0f, 0.0f);
+            this.bitangentX = 0.0f;
+            this.unknownShort1 = 0;
+            this.unknownInt1 = 0;
+            this.uvcoords = new UVCoord(0.0f, 0.0f);
+            this.normal = new Vector3(0.0f, 0.0f, 0.0f);
+            this.bitangentY = 0.0f;
+            this.tangent = new Vector3(0.0f, 0.0f, 0.0f);
+            this.bitangentZ = 0.0f;
+            this.vertexColors = new Color4(1.0f, 1.0f, 1.0f, 1.0f);
+            this.boneWeights = new float[4];
+            this.boneWeights[0] = 0.0f;
+            this.boneWeights[1] = 0.0f;
+            this.boneWeights[2] = 0.0f;
+            this.boneWeights[3] = 0.0f;
+            this.boneIndices = new byte[4];
+            this.boneIndices[0] = 0;
+            this.boneIndices[1] = 0;
+            this.boneIndices[2] = 0;
+            this.boneIndices[3] = 0;
+            this.unknownInt2 = 0;
+        }
+    }
+}

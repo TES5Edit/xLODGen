44bff0153c06ed0f51574f0e1d1215349998d46d
sheson <no@lol.comn>
2016-12-18 02:48:32 +0100

    LODGen 0.9
    added material 'PassThru' - works for BSLightingShaderProperty and BSEffectShaderProperty
    	entire shader will be copied straight into the bto
    	shapes with equal shaders are merged
    	textures will not be atlassed
    	changes all vertex alpha to 1f, since LOD only has alpha on/off at 50%, vertex alpha is applied if SLSF2_LOD_Objects/SLSF2_HD_LOD_Objects are not set
    	Extra Data List and Controllers are ignored
    	keep vertex colors regardless of --dontGenerateVertexColors
    added alternate texture replacement - see LODGen_alt_tamriel.txt / LODGen_alt_textures_tamriel.txt in  bin/Release
    	applied before atlas
    	Get 3D Index and 3D Name and replacement textures from CK - both 3D Index and 3D Name need to match in nif
    	if source texture has \lod in path the desination must have it too and vice versa
    generate normals in case they are missing from a shape
    read tangents/bitangents from binary extra data, Oblivion

---
+++ AltTextureDesc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LODGenerator
+{
+    public class AltTextureDesc
+    {
+        public string[] textures;
+
+        public AltTextureDesc()
+        {
+            this.textures = new string[9];
+        }
+    }
+
+    static class AltTextureList
+    {
+        static Dictionary<string, AltTextureDesc> _list;
+
+        static AltTextureList()
+        {
+            _list = new Dictionary<string, AltTextureDesc>();
+        }
+
+        public static void Set(string key, AltTextureDesc value)
+        {
+            _list.Add(key, value);
+        }
+
+        public static AltTextureDesc Get(string key)
+        {
+            return _list[key];
+        }
+
+        public static bool Contains(string key)
+        {
+            return _list.ContainsKey(key);
+        }
+    }
+}
